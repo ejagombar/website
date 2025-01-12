cd059e340215188b13094ba57ed2a7236e6b045d
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2025-01-12 12:04:35 +0000

    Move to single page site

--- scripts.js
+++ scripts.js
@@ -16,33 +16,113 @@ function myFunction() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    var x = document.getElementById('myLinks')
-    var menuToggle = document.getElementById('menu-toggle')
-    x.style.display = 'none'
-    menuToggle.checked = false
+document.addEventListener('DOMContentLoaded', () => {
+    const navItems = document.querySelectorAll('nav a')
+    const contentContainer = document.getElementById('content')
+
+    // Cache for fetched data
+    const cache = {
+        iconsData: null,
+        projectsData: null,
+    }
+
+    // Mock functions to fetch data
+    async function loadIconsData() {
+        if (!cache.iconsData) {
+            try {
+                const response = await fetch('data/icons.json')
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                cache.iconsData = await response.json()
+            } catch (error) {
+                console.error(
+                    'There was a problem with fetching the icons data:',
+                    error
+                )
+            }
+        }
+        return cache.iconsData
+    }
+
+    async function loadProjectData() {
+        if (!cache.projectsData) {
+            try {
+                const response = await fetch('data/projects.json')
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                cache.projectsData = await response.json()
+            } catch (error) {
+                console.error(
+                    'There was a problem with fetching the project data:',
+                    error
+                )
+            }
+        }
+        return cache.projectsData
+    }
+
+    // Define pages with placeholders for dynamic content
+    const pages = {
+        home: `
+                <div class="profileBox">
+                    <div class="profileLayout1">
+                        <img class="profilePicture" src="data/profilePic.jpg" width="200px" height="200px" alt="Profile Picture" />
+                        <div class="profileLayout2">
+                            <p>
+                                Hi, I am a recent <b>Electronic and Computer Engineering</b> graduate from the University of Nottingham.
+                            </p>
+                            <div id="link-icons-container" class="link-icons-container" > </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="aboutBox">
+                    <p class="aboutParagraph">
+                        Hi, I am an Electronic and Computer Engineer, looking for a job in software engineering. I recently achieved a First class degree in my Master's at the University of Nottingham. <br /><br />
+
+                        In my free time, I enjoy working on software projects and building stuff. Some recent examples can be found under the portfolio page. I like to try new things and explore new areas, such as my current project, WikiMapper, where I am processing Wikipedia XML dumps with C++ to create a visual graph database. <br /><br />
+
+                        Now that I have graduated, I want to focus on backend development, improving my knowledge of Linux, C++, and computer architecture. <br /><br />
+
+                        In my free time, I enjoy Mounting Biking, Climbing, Guitar, Cooking, and building things.
+                    </p>
+                </div>
+            `,
+        projects: `
+                <div id="projectsContainer"> </div>
+                `,
+    }
+
+    // Load the requested page content
+    async function loadPage(page) {
+        contentContainer.innerHTML = pages[page]
+        if (page === 'home') {
+            const iconsData = await loadIconsData()
+            renderIcons(iconsData)
+        }
+
+        if (page === 'projects') {
+            const iconsData = await loadProjectData()
+            renderProjects(iconsData)
+        }
+    }
+
+    // Add click event listeners to navigation links
+    navItems.forEach((item) => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault()
+            const page = item.getAttribute('href').replace('#', '')
+            loadPage(page)
+        })
+    })
+
+    // Load the default page (home)
+    loadPage('home')
 })
 
-loadIconsData()
-loadProjectData()
-
 window.addEventListener('resize', myFunction2)
-
-async function loadIconsData() {
-    try {
-        const response = await fetch('data/icons.json')
-        if (!response.ok) {
-            throw new Error('Network response was not ok')
-        }
-        const iconsData = await response.json()
-        renderIcons(iconsData)
-    } catch (error) {
-        console.error(
-            'There was a problem with fetching the icons data:',
-            error
-        )
-    }
-}
 
 function renderIcons(iconsData) {
     const container = document.getElementById('link-icons-container') // Make sure you have an element with this ID
@@ -80,22 +160,6 @@ function renderIcons(iconsData) {
             container.appendChild(spacer)
         }
     })
-}
-
-async function loadProjectData() {
-    try {
-        const response = await fetch('data/projects.json')
-        if (!response.ok) {
-            throw new Error('Network response was not ok')
-        }
-        const iconsData = await response.json()
-        renderProjects(iconsData)
-    } catch (error) {
-        console.error(
-            'There was a problem with fetching the project data:',
-            error
-        )
-    }
 }
 
 function renderProjects(projects) {
@@ -166,5 +230,54 @@ function renderProjects(projects) {
 
         projectBox.appendChild(projectBody)
         container.appendChild(projectBox)
+
+        projectBox.addEventListener('click', async () => {
+            await expandProjectBox(projectBox, project.id)
+        })
     })
+}
+
+async function loadProjectDetails(projectId) {
+    try {
+        const response = await fetch('data/projectDetails.json')
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+        const detailsData = await response.json()
+        return detailsData.find((detail) => detail.id === projectId)
+    } catch (error) {
+        console.error(
+            'There was a problem with fetching the project details:',
+            error
+        )
+    }
+}
+
+async function expandProjectBox(projectBox, projectId) {
+    const projectBody = projectBox.querySelector('.projectBody')
+    projectBox.classList.add('expanded')
+
+    // Fade out old content
+    projectBody.style.opacity = 0
+
+    // Load detailed description
+    const details = await loadProjectDetails(projectId)
+
+    if (details) {
+        setTimeout(() => {
+            // Replace old content with detailed description
+            projectBody.innerHTML = '' // Clear current content
+
+            const detailedDescription = document.createElement('p')
+            detailedDescription.classList.add('detailedText')
+            detailedDescription.innerHTML = details.description.replace(
+                /\n/g,
+                '<br>'
+            )
+            projectBody.appendChild(detailedDescription)
+
+            // Fade in new content
+            projectBody.style.opacity = 1
+        }, 300) // Match this duration with the CSS transition duration
+    }
 }
