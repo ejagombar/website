9a68d362eb4124a4e57e9c263b0d0b45d7fc7980
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2025-01-13 20:14:48 +0000

    Add icon to make it clear that project boxes can be clicked

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ const cache = {
     projectDetails: null,
 }
 
-async function loadProjectDetails(projectId) {
+async function loadProjectDetails(projectTitle) {
     if (!cache.projectDetails) {
         try {
             const response = await fetch('data/projectDetails.json')
@@ -17,7 +17,7 @@ async function loadProjectDetails(projectId) {
             )
         }
     }
-    return cache.projectDetails.find((detail) => detail.id === projectId)
+    return cache.projectDetails.find((detail) => detail.title === projectTitle)
 }
 
 export function renderProjects(projects) {
@@ -30,6 +30,32 @@ export function renderProjects(projects) {
         const title = document.createElement('h2')
         title.textContent = project.title
         projectBox.appendChild(title)
+
+        const svgIcon = document.createElementNS(
+            'http://www.w3.org/2000/svg',
+            'svg'
+        )
+        svgIcon.classList.add('projectIcon')
+        svgIcon.setAttribute('width', '20')
+        svgIcon.setAttribute('height', '20')
+        svgIcon.setAttribute('fill', 'var(--highlight)')
+        svgIcon.setAttribute('viewBox', '0 0 20 20')
+
+        const path = document.createElementNS(
+            'http://www.w3.org/2000/svg',
+            'path'
+        )
+        path.setAttribute(
+            'd',
+            'M10 20A10 10 0 1 0 0 10a10 10 0 0 0 10 10zM8.711 4.3l5.7 5.766L8.7 15.711l-1.4-1.422 4.289-4.242-4.3-4.347z'
+        )
+
+        svgIcon.appendChild(path)
+        title.appendChild(svgIcon)
+
+        title.style.display = 'flex'
+        title.style.alignItems = 'center'
+        title.style.gap = '8px'
 
         const projectBody = document.createElement('div')
         projectBody.classList.add('projectBody')
@@ -53,108 +79,82 @@ export function renderProjects(projects) {
         })
         projectDivider.appendChild(labelBox)
 
-        const linkBox = document.createElement('div')
-        linkBox.classList.add('projectLinkBox')
-        project.links.forEach((link) => {
-            const linkElement = document.createElement('a')
-            linkElement.classList.add('projectLink')
-            linkElement.href = link.url
-            linkElement.target = '_blank'
-            linkElement.textContent = link.label
+        // const linkBox = document.createElement('div')
+        // linkBox.classList.add('projectLinkBox')
+        // project.links.forEach((link) => {
+        //     const linkElement = document.createElement('a')
+        //     linkElement.classList.add('projectLink')
+        //     linkElement.href = link.url
+        //     linkElement.target = '_blank'
+        //     linkElement.textContent = link.label
+        //
+        //     const svgIcon = document.createElementNS(
+        //         'http://www.w3.org/2000/svg',
+        //         'svg'
+        //     )
+        //     svgIcon.setAttribute('width', '28')
+        //     svgIcon.setAttribute('height', '1.5em')
+        //     svgIcon.setAttribute('fill', 'currentColor')
+        //     svgIcon.setAttribute('viewBox', '0 0 16 16')
+        //
+        //     const path = document.createElementNS(
+        //         'http://www.w3.org/2000/svg',
+        //         'path'
+        //     )
+        //     path.setAttribute(
+        //         'd',
+        //         'M10.604 1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03 9.28 7.78a.75.75 0 0 1-1.06-1.06l3.75-3.75-1.543-1.543A.25.25 0 0 1 10.604 1zM3.75 2A1.75 1.75 0 0 0 2 3.75v8.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0 0 14 12.25v-3.5a.75.75 0 0 0-1.5 0v3.5a.25.25 0 0 1-.25.25h-8.5a.25.25 0 0 1-.25-.25v-8.5a.25.25 0 0 1 .25-.25h3.5a.75.75 0 0 0 0-1.5h-3.5z'
+        //     )
 
-            const svgIcon = document.createElementNS(
-                'http://www.w3.org/2000/svg',
-                'svg'
-            )
-            svgIcon.setAttribute('width', '28')
-            svgIcon.setAttribute('height', '1.5em')
-            svgIcon.setAttribute('fill', 'currentColor')
-            svgIcon.setAttribute('viewBox', '0 0 16 16')
-
-            const path = document.createElementNS(
-                'http://www.w3.org/2000/svg',
-                'path'
-            )
-            path.setAttribute(
-                'd',
-                'M10.604 1h4.146a.25.25 0 0 1 .25.25v4.146a.25.25 0 0 1-.427.177L13.03 4.03 9.28 7.78a.75.75 0 0 1-1.06-1.06l3.75-3.75-1.543-1.543A.25.25 0 0 1 10.604 1zM3.75 2A1.75 1.75 0 0 0 2 3.75v8.5c0 .966.784 1.75 1.75 1.75h8.5A1.75 1.75 0 0 0 14 12.25v-3.5a.75.75 0 0 0-1.5 0v3.5a.25.25 0 0 1-.25.25h-8.5a.25.25 0 0 1-.25-.25v-8.5a.25.25 0 0 1 .25-.25h3.5a.75.75 0 0 0 0-1.5h-3.5z'
-            )
-            svgIcon.appendChild(path)
-            linkElement.appendChild(svgIcon)
-
-            linkBox.appendChild(linkElement)
-        })
-        projectBody.appendChild(linkBox)
-
-        const button = document.createElement('button')
-        projectBody.appendChild(button)
+        //     svgIcon.appendChild(path)
+        //     linkElement.appendChild(svgIcon)
+        //
+        //     linkBox.appendChild(linkElement)
+        // })
+        // projectBody.appendChild(linkBox)
 
         projectBox.appendChild(projectBody)
         container.appendChild(projectBox)
 
-        button.addEventListener('click', () => {
-            const newContent =
-                'ThiThis is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longercontent that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!This is some longer content that will increase the height of the div!s is some longer content that will increase the height of the div!'
-            // Get the current height of the div
-            const currentHeight = projectBody.offsetHeight
-
-            // Fade out the current content
-            const currentContent = projectBody
-            currentContent.classList.remove('visible')
-            currentContent.classList.add('hidden')
-
-            // Set the height to the current height, to allow transition
-            projectBody.style.height = `${currentHeight}px`
-
-            // After fade out completes, update content and animate height change
-            setTimeout(() => {
-                // Step 1: Change the content of the div
-                currentContent.innerHTML = newContent
-
-                // Step 2: Fade in the new content
-                currentContent.classList.remove('hidden')
-                currentContent.classList.add('visible')
-
-                // Step 3: Get the new height of the content
-                const newHeight = projectBody.scrollHeight
-
-                // Step 4: Animate the height change
-                requestAnimationFrame(() => {
-                    projectBody.style.height = `${newHeight}px`
-                })
-            }, 500) // Match the fade-out duration in CSS (0.5s)
-
-            // projectBox.addEventListener('click', async () => {
-            //     console.log('clikd')
-            //     await expandProjectBox(projectBox, project.id)
-            // })
-        })
+        projectBox.addEventListener('click', (event) =>
+            handleProjectClick(event, projectBody, title.textContent)
+        )
     })
 
-    loadProjectDetails() // Load project details in the background on this page
+    loadProjectDetails()
 }
 
-async function expandProjectBox(projectBox, projectId) {
-    const projectBody = projectBox.querySelector('.projectBody')
-    projectBox.classList.add('expanded')
+function handleProjectClick(event, projectBody, title) {
+    event.preventDefault()
 
-    projectBody.style.opacity = 0
+    let newContent
+    loadProjectDetails(title).then(
+        function (value) {
+            console.log(value)
+            newContent = value.description
+        },
+        function (error) {
+            console.log(error)
+        }
+    )
+    console.log(newContent)
+    const currentHeight = projectBody.offsetHeight
 
-    const details = await loadProjectDetails(projectId)
+    const currentContent = projectBody
+    currentContent.classList.remove('visible')
+    currentContent.classList.add('hidden')
 
-    if (details) {
-        setTimeout(() => {
-            projectBody.innerHTML = ''
+    projectBody.style.height = `${currentHeight}px`
 
-            const detailedDescription = document.createElement('p')
-            detailedDescription.classList.add('detailedText')
-            detailedDescription.innerHTML = details.description.replace(
-                /\n/g,
-                '<br>'
-            )
-            projectBody.appendChild(detailedDescription)
+    setTimeout(() => {
+        currentContent.innerHTML = newContent
+        currentContent.classList.remove('hidden')
+        currentContent.classList.add('visible')
 
-            projectBody.style.opacity = 1
-        }, 300)
-    }
+        const newHeight = projectBody.scrollHeight
+
+        requestAnimationFrame(() => {
+            projectBody.style.height = `${newHeight}px`
+        })
+    }, 300)
 }
